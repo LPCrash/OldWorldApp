0e74d7e85f6a9349f82ebf631b5983841be0b8d3
LPCrash <gr.forum@gmx.de>
2025-07-26 10:18:02 +0200

    Expandable Panels added, Stubborn added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -14,6 +14,124 @@ sap.ui.define(
 				var oData = this.getView().getModel().getData();
 
 				// Reset data before calculating result
+				this.resetInputData(oData);
+				this.byId("idInputCombatResult").setValueState("None");
+				this.byId("idInputCombatResult").setValue("");
+
+				// Calculate Combat Result
+				this.calculateCombatResult(oData);
+
+				if (oData.combatResult == 0) {
+					this.byId("idInputCombatResult").setValueState(
+						"Information"
+					);
+					this.byId("idInputCombatResult").setValue(
+						this.getView()
+							.getModel("i18n")
+							.getResourceBundle()
+							.getText("placeholderDraw")
+					);
+				} else if (oData.combatResult > 0) {
+					this.byId("idInputCombatResult").setValueState("Success");
+					this.byId("idInputCombatResult").setValue(
+						this.getView()
+							.getModel("i18n")
+							.getResourceBundle()
+							.getText("placeholderWin") +
+							": +" +
+							oData.combatResult
+					);
+				} else {
+					this.byId("idInputCombatResult").setValueState("Error");
+					this.byId("idInputCombatResult").setValue(
+						this.getView()
+							.getModel("i18n")
+							.getResourceBundle()
+							.getText("placeholderLoss") +
+							": " +
+							oData.combatResult
+					);
+				}
+
+				this.byId("idInputCombatResult").setVisible(true);
+
+				if (oData.combatResult !== 0) {
+					this.byId("idPanelCombatResult").setExpanded(false);
+					this.byId("idPanelBreaktest").setExpanded(true);
+				}
+			},
+
+			onButtonCalcBreakTestPressed(oEvent) {
+				var oData = this.getView().getModel().getData();
+
+				// Reset data before calculating result
+				oData.breaktestModifiedDiceroll = 0;
+
+				var oBreakTestResult = this.calculateBreaktest(oData);
+				switch(oBreakTestResult) {
+					case "Flee":
+						this.byId("idInputBreakTest").setValueState("Error");
+						this.byId("idInputBreakTest").setValue(
+							this.getView()
+								.getModel("i18n")
+								.getResourceBundle()
+								.getText("placeholderFlee")
+						);
+						this.byId("idInputBreakTest").setVisible(true);
+						break;
+
+					case "FBiGO":
+						this.byId("idInputBreakTest").setValueState("Warning");
+						this.byId("idInputBreakTest").setValue(
+							this.getView()
+								.getModel("i18n")
+								.getResourceBundle()
+								.getText("placeholderFallBack")
+						);
+						this.byId("idInputBreakTest").setVisible(true);
+						break;
+					
+					// Give Ground
+					default:
+						
+						this.byId("idInputBreakTest").setValueState(
+							"Information"
+						);
+						this.byId("idInputBreakTest").setValue(
+							this.getView()
+								.getModel("i18n")
+								.getResourceBundle()
+								.getText("placeholderGiveGround")
+						);
+						this.byId("idInputBreakTest").setVisible(true);
+				}
+			},
+
+			onPanelCombatResultExpand(oEvent) {
+				var oExpend = oEvent.getParameter("expand");
+				
+				if(oExpend) {
+					this.byId("idPanelBreaktest").setExpanded(false);
+				}
+				else {
+					this.byId("idPanelBreaktest").setExpanded(true);
+				}
+
+			},
+
+			onPanelBreakTestExpand(oEvent) {
+				var oExpend = oEvent.getParameter("expand");
+				
+				if(!oExpend) {
+					this.byId("idPanelCombatResult").setExpanded(true);
+				}
+				else {
+					this.byId("idPanelCombatResult").setExpanded(false);
+				}
+
+			},
+
+			resetInputData(oData) {
 				oData.unsavedWoundsDiff = 0;
 				oData.rankBonusDiff = 0;
 				oData.standardDiff = 0;
@@ -23,9 +141,9 @@ sap.ui.define(
 				oData.highGroundDiff = 0;
 				oData.overkillDiff = 0;
 				oData.othersDiff = 0;
-				this.byId("idInputCombatResult").setValueState("None");
-				this.byId("idInputCombatResult").setPlaceholder("");
+			},
 
+			calculateCombatResult(oData) {
 				oData.unsavedWoundsDiff =
 					oData.unsavedWoundsAttacker - oData.unsavedWoundsDefender;
 				oData.rankBonusDiff =
@@ -79,74 +197,32 @@ sap.ui.define(
 					oData.highGroundDiff +
 					oData.overkillDiff +
 					oData.othersDiff;
-
-				if (oData.combatResult == 0) {
-					this.byId("idInputCombatResult").setValueState(
-						"Information"
-					);
-					this.byId("idInputCombatResult").setPlaceholder(
-						this.getView()
-							.getModel("i18n")
-							.getResourceBundle()
-							.getText("placeholderDraw")
-					);
-				} else if (oData.combatResult > 0) {
-					this.byId("idInputCombatResult").setValueState("Success");
-					this.byId("idInputCombatResult").setPlaceholder(
-						this.getView()
-							.getModel("i18n")
-							.getResourceBundle()
-							.getText("placeholderWin") +
-							": +" +
-							oData.combatResult
-					);
-				} else {
-					this.byId("idInputCombatResult").setValueState("Error");
-					this.byId("idInputCombatResult").setPlaceholder(
-						this.getView()
-							.getModel("i18n")
-							.getResourceBundle()
-							.getText("placeholderLoss") +
-							": " +
-							oData.combatResult
-					);
-				}
-
-				this.byId("idInputCombatResult").setVisible(true);
-
-				if (oData.combatResult !== 0) {
-					this.byId("idListBreaktest").setVisible(true);
-				}
 			},
 
-			onButtonCalcBreakTestPressed(oEvent) {
-				var oData = this.getView().getModel().getData();
-
-				// Reset data before calculating result
-				oData.breaktestModifiedDiceroll = 0;
-
+			calculateBreaktest(oData) {
 				// If the result of the natural roll is higher than the unit's Leadership, the unit 'Breaks' and flees.
 				// If the result of the natural roll is equal to or lower than the unit's Leadership,
 				// but the modified result is higher than the unit's Leadership, the unit Falls Back in Good Order.
 				// If the modified result is equal to or lower than the unit's Leadership,
 				// or if the roll is a natural double 1, the unit Gives Ground.
-				//
-				// TODO Stubborn
-				if (
-					oData.breaktestNaturalDiceroll > oData.leadershipLoser &&
+				var oBreakTestResult = {
+					Flee: 	"Flee",
+					Fbigo:	"FBiGO",
+					Give:	"GG"
+				};
+
+				if(oData.unbreakable)
+					return oBreakTestResult.Give;
+
+				if(oData.stubborn)
+					return oBreakTestResult.Fbigo;
+
+				if(oData.breaktestNaturalDiceroll > oData.leadershipLoser &&
 					oData.breaktestNaturalDiceroll > 2 &&
-					!oData.unbreakable
-				) {
-					// Flee
-					this.byId("idInputBreakTest").setValueState("Error");
-					this.byId("idInputBreakTest").setPlaceholder(
-						this.getView()
-							.getModel("i18n")
-							.getResourceBundle()
-							.getText("placeholderFlee")
-					);
-					this.byId("idInputBreakTest").setVisible(true);
-				} else {
+					!oData.unbreakable) {
+						return oBreakTestResult.Flee;
+				}
+				else {
 					var oCombatResult = 0;
 
 					if (Math.sign(oData.combatResult) == 1)
@@ -162,31 +238,13 @@ sap.ui.define(
 							oData.leadershipLoser &&
 						oData.breaktestNaturalDiceroll > 2 &&
 						!oData.unbreakable
-					) {
-						// Fall Back in Good Order
-						this.byId("idInputBreakTest").setValueState("Warning");
-						this.byId("idInputBreakTest").setPlaceholder(
-							this.getView()
-								.getModel("i18n")
-								.getResourceBundle()
-								.getText("placeholderFallBack")
-						);
-						this.byId("idInputBreakTest").setVisible(true);
-					} else {
-						// Give Ground
-						this.byId("idInputBreakTest").setValueState(
-							"Information"
-						);
-						this.byId("idInputBreakTest").setPlaceholder(
-							this.getView()
-								.getModel("i18n")
-								.getResourceBundle()
-								.getText("placeholderGiveGround")
-						);
-						this.byId("idInputBreakTest").setVisible(true);
-					}
+					)
+						return oBreakTestResult.Fbigo;
+					
+					else
+						return oBreakTestResult.Give;
 				}
-			},
+			}
 		});
 	}
 );
